954ccf655feced9738a407943c7c5b7bea820618
Dan Hammer <dan.s.hammer@gmail.com>
2012-05-25 21:12:11 -0700

    added docs

--- src/computing/dice.clj
+++ src/computing/dice.clj
@@ -1,14 +1,16 @@
-(ns stat-computing.dice
+(ns computing.dice
   (:use [cascalog.api]
         [clojure.contrib.combinatorics :only [subsets]]
-        [stat-computing.data :only [ndvi]])
+        [computing.data :only [ndvi]])
   (:require [incanter.core :as i]
             [incanter.io :as io]
             [incanter.stats :as s]
             [incanter.optimize :as optimize]
             [incanter.charts :as c]))
 
-;; DICE PROBLEM!!!
+;; Maximize entropy subject to constraints, specifically that the
+;; average equal the true average and the sum of the individual
+;; probabilities is equal to 1
 
 (defn prob-die [beta i]
   (let [exp-fn (fn [b x] (i/exp (* -1 x b)))]
@@ -26,8 +28,8 @@
         diff-seq (map i/abs (map #(- % true-avg) avg-seq))]
     (nth beta-range (.indexOf diff-seq (reduce min diff-seq)))))
 
-;; (defn prob-seq [true-avg]
-;;   (let [beta (true-beta true-avg)]
-;;     (map (partial prob-die beta) (range 1 7))))
+(defn prob-seq [true-avg]
+  (let [beta (true-beta true-avg)]
+    (map (partial prob-die beta) (range 1 7))))
 
 ;; http://www.bestinclass.dk/index.clj/2010/01/hadoop-feeding-reddit-to-hadoop.html
